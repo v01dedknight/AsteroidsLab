fed96cd73155677905ae253200ed8fe47f992d51
v01d <Kevin_Vearez@mail.ru>
2026-04-05 21:17:26 +0700

    Added Chronicle Manager

---
+++ ChroneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+public static class ChroneManager {
+  private static List<IChroneListener> _listenerList = new List<IChroneListener>();
+
+  public static void AddListener(IChroneListener listener) {
+    _listenerList.Add(listener);
+  }
+
+  public static void RemoveListener(IChroneListener listener) {
+    _listenerList.Remove(listener);
+  }
+
+  public static void MakeChroneTick() {
+    foreach (var listener in _listenerList) {
+      listener.OnChroneTick();
+    }
+  }
+}
